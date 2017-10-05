be7d49ea30a7dee7a99d501c86a6b4f23ff018fc
Alexander Agudelo <mario323qp@gmail.com>
2017-10-05 11:02:51 +1000

    - Migrated project to new structure. - Built new release

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,65 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+utils = require('gulp-util'),
+jshint = require('gulp-jshint');
+// var through = require('through2')
+var pump = require('pump');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var htmlmin = require('gulp-htmlmin');
+var babel = require('gulp-babel');
+
+// Include Gulp
+var dest = "dist/";
+
+// Include plugins
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
+});
 
 // Check the code quality
 gulp.task('qualitychecker', function(cb) {
-    return gulp.src([
-      '**/*.js',
-      '!node_modules/**/*.js'])
-    .pipe(jshint({esversion: 6}))
-    .pipe(jshint.reporter('default'))
-    .on('error', gutil.log);
+	return gulp.src([
+		'**/*.js',
+		'!node_modules/**/*.js',
+		'!dist/**/*.js',
+		'!Touch/components/**/*.js'])
+		.pipe(jshint({esversion: 6}))
+		.pipe(jshint.reporter('default'))
+		.on('error', utils.log);
 });
 
+
+gulp.task('js', function (cb) {
+  pump([
+				gulp.src('Touch/**/*.js'),
+				babel({"presets": ["env"]}),
+				uglify(),
+        gulp.dest(dest)
+    ],
+    cb
+  );
+});
+
+
+gulp.task('css', function(cb) {
+	pump([
+		gulp.src('Touch/**/*.css'),
+		uglifycss(),
+		gulp.dest(dest)
+	], cb);
+});
+
+
+gulp.task('html', function (cb) {
+  pump([
+				gulp.src('Touch/**/*.html'),
+				htmlmin({collapseWhitespace: true}),
+        gulp.dest(dest)
+    ],
+    cb
+  );
+});
+
+gulp.task('build', ['js', 'css', 'html']);
 gulp.task('default', ['qualitychecker']);
